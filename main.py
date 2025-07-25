d5f277b020984cdb46d4c75be9c3ddbaf157c0e3
Sultan Ipinmisho <78211637+sekonii@users.noreply.github.com>
2025-07-25 07:30:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# python web server
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'App is running on Railway!'
+
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    port = int(os.environ.get("PORT", 5000))  # Fallback to 5000 locally
+    app.run(debug=False, host='0.0.0.0', port=port)
