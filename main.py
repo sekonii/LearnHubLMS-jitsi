94629522b290431b57dc2b0c012b40c3ed186590
Sultan Ipinmisho <78211637+sekonii@users.noreply.github.com>
2025-07-25 07:20:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from flask import Flask
-import os
+from app import app
 
-app = Flask(__name__)
-
-# Your routes go here
-@app.route('/')
-def home():
-    return 'Hello, Railway! Your Flask app is live.'
-
-# This is the important part
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # Railway will provide the PORT env variable
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
