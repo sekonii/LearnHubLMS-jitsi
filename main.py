94e90e5c10adcaba93940b510975a5e94ae40fa0
Sultan Ipinmisho <78211637+sekonii@users.noreply.github.com>
2025-07-26 19:38:22 -0700

    Latest Update on main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'App is running on Railway!'
+from app import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Fallback to 5000 locally
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
