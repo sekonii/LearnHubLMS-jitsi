56e9d3b7dddf6c1259661c76463717999f4841ae
Sultan Ipinmisho <78211637+sekonii@users.noreply.github.com>
2025-07-29 18:08:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from app import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Fallback to 5000 locally
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
